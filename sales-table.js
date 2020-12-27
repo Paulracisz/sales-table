cf62e40a9620eb624dd27a51d196cbd650b3a2c2
Paul <paulieracisz@gmail.com>
2020-12-27 11:55:21 -0500

    finished sorting, time to refactor

--- sales-table.js
+++ sales-table.js
@@ -1,11 +1,10 @@
-
 var sales = document.getElementById("sales-header");
-var clickCounter = 0
-let selectedQuarter
-
+var salesPerson = document.getElementById("top-sales");
+var clickCounter = 0;
+let selectedQuarter;
 
 const tableDetail = [
-    { month: "January", sales: 50000, topSalesPerson: "Angela" },
+  { month: "January", sales: 50000, topSalesPerson: "Angela" },
   { month: "February", sales: 10000, topSalesPerson: "Roberto" },
   { month: "March", sales: 85000, topSalesPerson: "Maria" },
   { month: "April", sales: 56000, topSalesPerson: "Stacy" },
@@ -19,67 +18,82 @@ const tableDetail = [
   { month: "December", sales: 121000, topSalesPerson: "William" },
 ];
 
-let duplicateArray = tableDetail
-// modified duplicateArray
-// renderTable(duplicate)
+let duplicateArray = new Array(...tableDetail);
 
 function renderTable(tableDetails) {
-    document.getElementById("table-container").innerHTML = tableDetails.map(
-        (detail) =>
-          `<tr>
+  document.getElementById("table-container").innerHTML = tableDetails
+    .map(
+      (detail) =>
+        `<tr>
             <td class="text-left">${detail.month}</td>
-            <td class="text-left">${detail.sales}</td>
+            <td class="text-left">${detail.sales.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}</td>
             <td class="text-left">${detail.topSalesPerson}</td>
           </tr>`
-      ).join("");
+    )
+    .join("");
 }
 
-function onSortTable() {
+function onSortTable(whichSort) {
   clickCounter++;
 
-  const sortedArray = duplicateArray
-  sortedArray.sort((x, y) => { 
-    return x.sales - y.sales
-   })
+  const sortedArray = new Array(...duplicateArray);
+  
+  // conditional to see which heading was clicked to sort either sales person, or sales numbers
+
+  if (whichSort == "sales") {
+    sortedArray.sort((x, y) => {
+      return x.sales - y.sales;
+    });
+  } else if (whichSort == "salesperson") {
+    sortedArray.sort((a, b) => {
+        if ( a.topSalesPerson < b.topSalesPerson ){
+          return -1;
+        }
+        if ( a.topSalesPerson > b.topSalesPerson ){
+          return 1;
+        }
+        return 0;
+      });
+  }
 
   if (clickCounter == 1) {
-    renderTable(sortedArray)
+    renderTable(sortedArray);
   } else if (clickCounter == 2) {
     renderTable(sortedArray.reverse());
-    clickCounter = 0
   } else if (clickCounter == 3) {
-    // triggerFilter = current checked box 
+    renderTable(duplicateArray);
+    // triggerFilter = current checked box
     // if checked box = 1, make copy of duplicateArray.slice(0,3)
-    renderTable(duplicateArray)
     clickCounter = 0;
   }
 }
 
 function onFilter(triggerFilter) {
-    if (triggerFilter == selectedQuarter) {
-        renderTable(tableDetail)
-        duplicateArray = tableDetail
-    }
-    else if(triggerFilter != undefined){
-        var allFilterCheckboxes = document.querySelectorAll(".check")
-        allFilterCheckboxes.forEach(checkbox => checkbox.checked = false)
-        triggerFilter.checked = true
-        var quarter = triggerFilter.id.slice(1,2)  //(1, 2, 3, 4 )
-        // Reducing the ID of checkboxes which are Q1,Q2,Q3,Q4 into just the number so we can check
-        const quarterEnd = quarter * 3 // ( 3, 6, 9, 12)
-        const quarterStart = quarterEnd - 3
+  // Only triggers on page load and if box is clicked-
+  // twice to reset table to original value.
+  if (triggerFilter == selectedQuarter) {
+    renderTable(tableDetail);
+    duplicateArray = new Array(...tableDetail);
+    selectedQuarter = undefined;
+  }
+  // This conditional is met when a box is clicked.
+  else if (triggerFilter != undefined) {
+    clickCounter = 0;
+    let allFilterCheckboxes = document.querySelectorAll(".check");
+    allFilterCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+    triggerFilter.checked = true;
+    let quarter = triggerFilter.id.slice(1, 2); //(1, 2, 3, 4 )
+    // Reducing the ID of checkboxes which are Q1,Q2,Q3,Q4 into just the number so we can check
+    const quarterEnd = quarter * 3; // ( 3, 6, 9, 12)
+    const quarterStart = quarterEnd - 3;
+    let quarterArray = tableDetail.slice(quarterStart, quarterEnd);
+    renderTable(quarterArray);
+    duplicateArray = quarterArray; // make a workable copy for other operations.
+    selectedQuarter = triggerFilter; //selected quarter tracks to see if checkbox was selected again
+  }
+}
 
-        let quarterArray = tableDetail.slice(quarterStart, quarterEnd)
-        renderTable(quarterArray)
-        duplicateArray = quarterArray   //Creates workable array for other actions
-        selectedQuarter = triggerFilter //selected quarter tracks to see if checkbox was selected again
-    }
-  } 
-
-/*  
-    1> Select the Sales header
-    2> Create a function that tracks    
-
-
-*/
 renderTable(tableDetail);
